84fd3171bea260246fa2f54f1734f95728c1c593
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-10-26 00:49:46 +0200

    Refactor project

--- add.adb
+++ add.adb
@@ -22,80 +22,89 @@ package body add is
       end loop;
    end Background;
 
+   -----------------------------------------------------------------------
+   ------------- declaration of types
+   -----------------------------------------------------------------------
+
+   type Sintoma_Distancia_Type is (Segura, Insegura, Imprudente, Colision);
+
+   -----------------------------------------------------------------------
+   ------------- declaration of auxiliary methods
+   -----------------------------------------------------------------------
+
+   procedure Starting_Notice (Task_Name : in String);
+   procedure Finishing_Notice (Task_Name : in String);
+   function Number_Sign (Number : Integer) return Integer;
+   procedure Riesgo_Cabeza
+     (Cabeza      : in     HeadPosition_Samples_Type;
+      Volante     : in     Steering_Samples_Type; Prev_X_Risk : in Boolean;
+      Prev_Y_Risk : in     Boolean; X_Risk : out Boolean; Y_Risk : out Boolean;
+      Risk        :    out Boolean);
+   procedure Riesgo_Distancia
+     (Velocidad : in Speed_Samples_Type; Distancia : in Distance_Samples_Type;
+      Risk      :    out Sintoma_Distancia_Type);
+
    ----------------------------------------------------------------------
    ------------- declaration of protected objects
    ----------------------------------------------------------------------
 
    protected Sintomas is
-      procedure Run_Cabeza;
-      procedure Run_Volante;
+      procedure Update_Cabeza (Risk : Boolean);
+      procedure Update_Distancia (Risk : Sintoma_Distancia_Type);
+      procedure Run_Riesgos;
+      procedure Update_Volante (Risk : Boolean);
+      procedure Run_Display;
    private
-      Cabeza_Inclinada : Boolean := False;
-
-      Distancia_Insegura   : Boolean := False;
-      Distancia_Imprudente : Boolean := False;
-      Distancia_Colision   : Boolean := False;
+      Riesgo_Cabeza    : Boolean                := False;
+      Riesgo_Distancia : Sintoma_Distancia_Type := Segura;
+      Riesgo_Volante   : Boolean                := False;
    end Sintomas;
+
+   protected Medidas is
+      procedure Update_Distancia (Distancia : in Distance_Samples_Type);
+      procedure Update_Velocidad (Velocidad : in Speed_Samples_Type);
+      procedure Run_Riesgos;
+      procedure Run_Display;
+   private
+      Distancia : Distance_Samples_Type := 0;
+      Velocidad : Speed_Samples_Type    := 0;
+   end Medidas;
 
    ----------------------------------------------------------------------
    ------------- definition of protected objects
    ----------------------------------------------------------------------
 
    protected body Sintomas is
-      procedure Run_Cabeza is
-         Head_Position     : HeadPosition_Samples_Type;
-         Wheel_Position    : Steering_Samples_Type;
-         Current_X_Danger  : Boolean := False;
-         Current_Y_Danger  : Boolean := False;
-         Previous_X_Danger : Boolean := False;
-         Previous_Y_Danger : Boolean := False;
+      procedure Update_Cabeza (Risk : Boolean) is
       begin
-         loop
-            Reading_HeadPosition (Head_Position);
-            Reading_Steering (Wheel_Position);
+         Riesgo_Cabeza := Risk;
+      end Update_Cabeza;
 
-            Current_X_Danger := abs (Head_Position (x)) > 30;
-            Current_Y_Danger := abs (Head_Position (y)) > 30;
-
-            if (Current_X_Danger and Previous_X_Danger) or
-              (Current_Y_Danger and Previous_Y_Danger and
-               abs (Wheel_Position) <= 30 and
-               Number_Sign (Wheel_Position) /= Number_Sign (HeadPosition (y)))
-            then
-               Cabeza_Inclinada := True;
-            end if;
-
-            Previous_X_Danger := Current_X_Danger;
-            Previous_Y_Danger := Current_Y_Danger;
-         end loop;
-      end Run_Cabeza;
-
-      procedure Run_Volante is
-         Distance       : Distance_Samples_Type;
-         Velocity       : Speed_Samples_Type;
-         SecureVelocity : Integer := (Velocity / 10) * (Velocity / 10);
+      procedure Update_Distancia (Risk : Sintoma_Distancia_Type) is
+         Speed           : Speed_Samples_Type    := 0;
+         Distance        : Distance_Samples_Type := 0;
+         Safety_Distance : Float                 := 0.0;
       begin
-         loop
-            if Distance < SecureVelocity / 3 then
-               Distancia_Colision   := True;
-               Distancia_Imprudente := False;
-               Distancia_Insegura   := False;
-            elsif Distance < SecureVelocity / 2 then
-               Distancia_Colision   := False;
-               Distancia_Imprudente := True;
-               Distancia_Insegura   := False;
-            elsif Distance < SecureVelocity then
-               Distancia_Colision   := False;
-               Distancia_Imprudente := False;
-               Distancia_Insegura   := True;
-            else
-               Distancia_Colision   := False;
-               Distancia_Imprudente := False;
-               Distancia_Insegura   := False;
-            end if;
-         end loop;
-      end Run_Volante;
+         Riesgo_Distancia := Risk;
+      end Update_Distancia;
+
+      procedure Update_Volante (Risk : Boolean) is
+      begin
+         Riesgo_Volante := Risk;
+      end Update_Volante;
    end Sintomas;
+
+   protected body Medidas is
+      procedure Update_Distancia (Distancia : in Distance_Samples_Type) is
+      begin
+         Medidas.Distancia := Distancia;
+      end Update_Distancia;
+
+      procedure Update_Velocidad (Velocidad : in Speed_Samples_Type) is
+      begin
+         Medidas.Velocidad := Velocidad;
+      end Update_Velocidad;
+   end Medidas;
 
    -----------------------------------------------------------------------
    ------------- declaration of tasks
@@ -109,36 +118,113 @@ package body add is
    task Modo;
 
    -----------------------------------------------------------------------
-   ------------- declaration auxiliary methods
-   -----------------------------------------------------------------------
-
-   procedure Starting_Notice (Task_Name : in String);
-   procedure Finishing_Notice (Task_Name : in String);
-   function Number_Sign (Number : Integer) return Integer;
-
-   -----------------------------------------------------------------------
    ------------- body of tasks
    -----------------------------------------------------------------------
 
    task body Cabeza is
-      Task_Name   : constant String  := "Cabeza";
-      Task_Period : constant Natural := 400;
+      Task_Name   : constant String   := "Cabeza";
+      Task_Period : constant Duration := 0.400;
+
+      Head_Position  : HeadPosition_Samples_Type := (0, 0);
+      Steering_Angle : Steering_Samples_Type     := 0;
+      Prev_X_Risk    : Boolean                   := False;
+      Prev_Y_Risk    : Boolean                   := False;
+      X_Risk         : Boolean                   := False;
+      Y_Risk         : Boolean                   := False;
+      Head_Risk      : Boolean                   := False;
    begin
-      Starting_Notice (Task_Name);
-      Sintomas.Run_Cabeza;
-      Finishing_Notice (Task_Name);
-      delay (Task_Period);
+      loop
+         Starting_Notice (Task_Name);
+
+         Reading_HeadPosition (Head_Position);
+         Riesgo_Cabeza
+           (Head_Position, Steering_Angle, Prev_X_Risk, Prev_Y_Risk, X_Risk,
+            Y_Risk, Head_Risk);
+         Sintomas.Update_Cabeza (Head_Risk);
+
+         Finishing_Notice (Task_Name);
+         delay (Task_Period);
+      end loop;
    end Cabeza;
 
-   task body Volante is
-      task_name   : constant String  := "Volante";
-      task_period : constant Natural := 300;
+   task body Distancia is
+      task_name   : constant String   := "Distancia";
+      task_period : constant Duration := 0.300;
+
+      Distance_Risk : Sintoma_Distancia_Type := Segura;
+      Distance      : Distance_Samples_Type  := 0;
+      Speed         : Speed_Samples_Type     := 0;
    begin
-      Starting_Notice (task_name);
-      Sintomas.Run_Volante;
-      Finishing_Notice (task_name);
-      delay (Task_Period);
+      loop
+         Starting_Notice (task_name);
+
+         Reading_Speed (Speed);
+         Reading_Distance (Distance);
+
+         Riesgo_Distancia (Speed, Distance, Distance_Risk);
+         Sintomas.Update_Distancia (Distance_Risk);
+
+         -- Medidas.Update_Distancia;
+         -- Medidas.Update_Velocidad;
+
+         Finishing_Notice (task_name);
+         delay (task_period);
+      end loop;
+   end Distancia;
+
+   task body Volante is
+      task_name   : constant String   := "Volante";
+      task_period : constant Duration := 0.350;
+   begin
+      loop
+         Starting_Notice (task_name);
+         -- Sintomas.Update_Volante;
+         Finishing_Notice (task_name);
+         delay (task_period);
+      end loop;
    end Volante;
+
+   task body Riesgos is
+      task_name   : constant String   := "Riesgos";
+      task_period : constant Duration := 0.150;
+   begin
+      loop
+         Starting_Notice (task_name);
+
+         -- Task code here
+
+         Finishing_Notice (task_name);
+         delay (task_period);
+      end loop;
+   end Riesgos;
+
+   task body Display is
+      task_name   : constant String   := "Display";
+      task_period : constant Duration := 1.000;
+   begin
+      loop
+         Starting_Notice (task_name);
+
+         -- Task code here
+
+         Finishing_Notice (task_name);
+         delay (task_period);
+      end loop;
+   end Display;
+
+   task body Modo is
+      task_name   : constant String   := "Modo";
+      task_period : constant Duration := 0.3;
+   begin
+      loop
+         Starting_Notice (task_name);
+
+         -- Task code here
+
+         Finishing_Notice (task_name);
+         delay (task_period);
+      end loop;
+   end Modo;
 
    -----------------------------------------------------------------------
    ------------- body of auxiliary methods
@@ -146,12 +232,12 @@ package body add is
 
    procedure Starting_Notice (task_name : in String) is
    begin
-      Put ("Comenzando tarea " & task_name);
+      Put_Line ("Comenzando tarea " & task_name);
    end Starting_Notice;
 
    procedure Finishing_Notice (task_name : in String) is
    begin
-      Put ("Finalizando tarea " & task_name);
+      Put_Line ("Finalizando tarea " & task_name);
    end Finishing_Notice;
 
    function Number_Sign (Number : Integer) return Integer is
@@ -165,6 +251,56 @@ package body add is
 
       return Sign;
    end Number_Sign;
+
+   procedure Riesgo_Cabeza
+     (Cabeza      : in     HeadPosition_Samples_Type;
+      Volante     : in     Steering_Samples_Type; Prev_X_Risk : in Boolean;
+      Prev_Y_Risk : in     Boolean; X_Risk : out Boolean; Y_Risk : out Boolean;
+      Risk        :    out Boolean)
+   is
+      Current_X_Risk         : Boolean := False;
+      Current_Y_Risk         : Boolean := False;
+      Wheel_And_Head_Aligned : Boolean := False;
+      Total_X_Risk           : Boolean := False;
+      Total_Y_Risk           : Boolean := False;
+   begin
+      Current_X_Risk := abs (Cabeza (x)) > 30;
+      Current_Y_Risk := abs (Cabeza (y)) > 30;
+
+      Total_X_Risk := Current_X_Risk and Prev_X_Risk;
+
+      Wheel_And_Head_Aligned :=
+        Number_Sign (Integer (Volante)) = Number_Sign (Integer (Cabeza (y)));
+      Total_Y_Risk           :=
+        Current_Y_Risk and Prev_Y_Risk and abs (Volante) <= 30 and
+        not Wheel_And_Head_Aligned;
+
+      if Total_X_Risk or Total_Y_Risk then
+         Risk := True;
+      end if;
+
+      X_Risk := Current_X_Risk;
+      Y_Risk := Current_Y_Risk;
+   end Riesgo_Cabeza;
+
+   procedure Riesgo_Distancia
+     (Velocidad : in Speed_Samples_Type; Distancia : in Distance_Samples_Type;
+      Risk      :    out Sintoma_Distancia_Type)
+   is
+      Safety_Distance : Float := 0.0;
+   begin
+      Safety_Distance := (Float (Velocidad) / 10.0)**2;
+
+      if Float (Distancia) < Safety_Distance / 3.0 then
+         Risk := Colision;
+      elsif Float (Distancia) < Safety_Distance / 2.0 then
+         Risk := Imprudente;
+      elsif Float (Distancia) < Safety_Distance then
+         Risk := Insegura;
+      else
+         Risk := Segura;
+      end if;
+   end Riesgo_Distancia;
 
    ----------------------------------------------------------------------
    ------------- procedure para probar los dispositivos
